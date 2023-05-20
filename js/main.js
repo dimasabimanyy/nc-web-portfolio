59147bc7b41be893d7b1442078d4c52e4c393d29
Dimas Abimanyu <dmsabmny@gmail.com>
2023-05-20 11:39:07 +0700

    clear form before unload, fix formspree missing name input

--- js/main.js
+++ js/main.js
@@ -1,12 +1,9 @@
-// Navigation
-
+// Toggle & Responsive Navigation
 const navSlide = () => {
   const burger = document.querySelector(".burger");
   const navLists = document.querySelector("nav");
 
   burger.addEventListener("click", () => {
-    console.log("bro");
-
     // Toggle nav list and burger class
     navLists.classList.toggle("nav-active");
     burger.classList.toggle("toggle-burger");
@@ -14,3 +11,10 @@ const navSlide = () => {
 };
 
 navSlide();
+
+// Clear form before unload
+window.onbeforeunload = () => {
+  for (const form of document.getElementsByTagName("form")) {
+    form.reset();
+  }
+};
